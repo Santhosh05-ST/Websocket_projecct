38b231717a909ce84ca9d39e41ee28b6d263e743
Santhosh05-ST <santhoshsivetbcab@gmail.com>
2026-08-07 10:24:29 +0530

    first commit

---
+++ src/main/java/org/example/videochat/VideochatApplication.java
@@ -0,0 +1,13 @@
+package org.example.videochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideochatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideochatApplication.class, args);
+    }
+
+}
